3813ccb4f219dacccedd1b189d06f2a34a6ecb24
Juan Jose Sanchez <juanjsanchezcea@gmail.com>
2024-10-02 21:24:24 -0500

    Consumo de stored procedure

---
+++ Models/Gestor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionSaldos.Models
+{
+	public class Gestor
+	{
+		public int GestorId { get; set; }
+		public decimal TotalSaldos { get; set; }
+
+		public Gestor(int id)
+		{
+			GestorId = id;
+			TotalSaldos = 0;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+	static void Main()
+	{
+		string connectionString = @"Server=DESKTOP-9AGBIEE\SQLEXPRESS;Database=GestionCobrosDB;Integrated Security=True;"; ;
+
+		using (SqlConnection connection = new SqlConnection(connectionString))
+		{
+			connection.Open();
+			SqlCommand command = new SqlCommand("SP_asignarSaldosAGestores", connection);
+			command.CommandType = CommandType.StoredProcedure;
+
+			using (SqlDataReader reader = command.ExecuteReader())
+			{
+				while (reader.Read())
+				{
+					Console.WriteLine($"GestorId: {reader["GestorId"]}, TotalSaldos: {reader["TotalSaldos"]}");
+				}
+			}
+		}
+	}
+}
